d67c7f08f4a770c64e749c417fb9c3f29744aecf
zDayCoder <ryan.arruda.figueiredo@gmail.com>
2024-05-22 22:00:51 -0300

    Dev: Atualizando rotas 22/05/2024

--- routes/register.php
+++ routes/register.php
@@ -5,4 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
 
 Route::post('/register', [RegisterController::class, 'registerIntern'])->name('register.intern');
+Route::post('/register', [RegisterController::class, 'registerEnterprise'])->name('register.enterprise');
+Route::post('/register', [RegisterController::class, 'registerCoordinator'])->name('register.coordinator');
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
