e1c708077b747f6c070002a885a5d006811f32a3
Lucas Lourenço <lucas.g.j.l@hotmail.com>
2024-06-17 18:36:47 -0300

    Final

--- resources/views/components/dashboard-vagas.blade.php
+++ resources/views/components/dashboard-vagas.blade.php
@@ -1,15 +1,18 @@
-<div class="card">
-<div class="container-vagas">
-    <div class="title-vagas"> <h1>Feed de Vagas</h1></div>
-    <div class="resume-job-card" onclick="showDetails()">
-        <x-job-component />
+<div class="card mt-5">
+    <div class="title-vagas2">
+        <h1>Feed de Vagas</h1>
     </div>
-    <div class="detalhes-container">
-        <x-job-details-component />
-    </div>
+    <div class="container-vagas">
+       
+        <div class="resume-job-card" onclick="showDetails()">
+            <x-job-component />
+        </div>
+        <div class="detalhes-container">
+            <x-job-details-component />
+        </div>
 
-  
-</div>
+
+    </div>
 
 </div>
 
